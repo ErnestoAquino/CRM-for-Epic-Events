376b67080c0d1882f2494b923993f213894cba0e
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-01-25 21:33:23 +0100

    chore: add 'crm' app to installed apps in Django settings

--- EpicCRM/EpicCRM/settings.py
+++ EpicCRM/EpicCRM/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crm',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'epic_crm.Collaborator'
