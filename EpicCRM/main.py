9bdd8879e3836424ec87f6713fa7c70fd7175e24
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-01-30 17:49:49 +0100

    feat: add main.py for CRM application execution

---
+++ EpicCRM/main.py
@@ -0,0 +1,10 @@
+from controllers.ControllerCRM import ControllerCRM
+
+
+def main():
+    controller = ControllerCRM()
+    controller.create_collaborator()
+
+
+if __name__ == "__main__":
+    main()
