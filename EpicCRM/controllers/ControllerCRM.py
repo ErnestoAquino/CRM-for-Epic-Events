f7314a60656178c16766f3b5b11d17388d3e5e7f
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-01-30 17:45:25 +0100

    feat: implement create_collaborator method

--- EpicCRM/controllers/ControllerCRM.py
+++ EpicCRM/controllers/ControllerCRM.py
@@ -1,7 +1,18 @@
 from EpicCRM.services.ServicesCRM import ServicesCRM
 
+from EpicCRM.views.ViewCLI import ViewCLI
 
-class MainController:
-    def __int__(self):
-        self.crm_manager = ServicesCRM()
 
+class ControllerCRM:
+    def __init__(self):
+        self.crm_services = ServicesCRM()
+        self.view_cli = ViewCLI()
+
+    def create_collaborator(self):
+        data = self.view_cli.get_data_for_register_new_collaborator()
+
+        try:
+            self.crm_services.register_collaborator(**data)
+            self.view_cli.print_message("User registered successfully", "green")
+        except ValueError as e:
+            self.view_cli.print_message(f"Error: {e}", "red")
