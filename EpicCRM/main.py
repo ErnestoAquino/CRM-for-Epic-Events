c1584fd8f6de17a61d46d7f213a0e21159f74e31
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-01-31 14:05:42 +0100

    feat: initialize Django ORM in main script

--- EpicCRM/main.py
+++ EpicCRM/main.py
@@ -1,3 +1,16 @@
+import os
+import django
+
+
+def setup_django():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EpicCRM.settings")
+    django.setup()
+
+
+setup_django()
+
+from views.ViewCLI import ViewCLI
+from services.ServicesCRM import ServicesCRM
 from controllers.ControllerCRM import ControllerCRM
 
 
