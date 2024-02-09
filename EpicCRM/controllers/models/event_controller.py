31ac4a688757d96c195521bd720558f2ba16dac9
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-09 17:08:37 +0100

    feat: Implement get_all_events method with permission check

--- EpicCRM/controllers/models/event_controller.py
+++ EpicCRM/controllers/models/event_controller.py
@@ -1,7 +1,19 @@
+from django.core.exceptions import PermissionDenied
+
 from crm.models import Collaborator
+from crm.models import Event
+
+from services.services_crm import ServicesCRM
 
 
 class EventController:
     def __init__(self, collaborator: Collaborator):
         self.collaborator = collaborator
+        self.service = ServicesCRM()
+
+    def get_all_events(self):
+        if self.collaborator.has_perm("crm.view_event"):
+            return self.service.get_all_events()
+        else:
+            raise PermissionDenied("You do not have permission to view the list of events.")
 
