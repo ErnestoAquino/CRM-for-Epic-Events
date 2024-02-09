de3505ad88547dc750fd090cd097ae5999b283bd
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-09 17:07:11 +0100

    feat: Implement get_all_clients method with permission check

--- EpicCRM/controllers/models/client_controller.py
+++ EpicCRM/controllers/models/client_controller.py
@@ -1,6 +1,16 @@
+from django.core.exceptions import PermissionDenied
 from crm.models import Collaborator
+from crm.models import Client
+from services.services_crm import ServicesCRM
 
 
 class ClientController:
     def __init__(self, collaborator: Collaborator):
         self.collaborator = collaborator
+        self.services = ServicesCRM()
+
+    def get_all_clients(self):
+        if self.collaborator.has_perm("crm.view_client"):
+            return self.services.get_all_clients()
+        else:
+            raise PermissionDenied("You do not have permission to view the list of clients.")
