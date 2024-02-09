82f1344d286cbb3fc6a4a6bce1359093c8a5e3b9
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-09 17:07:58 +0100

    feat: Implement get_all_contracts method with permission check

--- EpicCRM/controllers/models/contract_controller.py
+++ EpicCRM/controllers/models/contract_controller.py
@@ -1,6 +1,17 @@
+from django.core.exceptions import PermissionDenied
+
 from crm.models import Collaborator
+from crm.models import Contract
+from services.services_crm import ServicesCRM
 
 
 class ContractController:
     def __init__(self, collaborator: Collaborator):
         self.collaborator = collaborator
+        self.services = ServicesCRM()
+
+    def get_all_contracts(self):
+        if self.collaborator.has_perm("crm.view_contract"):
+            return self.services.get_all_contracts()
+        else:
+            raise PermissionDenied("You do not have permission to view the list of contracts.")
