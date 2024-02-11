912f0f4963c1b3d738dd84b27631b30ff2b44460
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-11 22:39:25 +0100

    feat: Add method to fetch event by ID with permission validation

--- EpicCRM/controllers/models/event_controller.py
+++ EpicCRM/controllers/models/event_controller.py
@@ -23,3 +23,30 @@ class EventController:
         else:
             raise PermissionDenied("You do not have permission to view the events.")
 
+    def get_event_by_id(self, event_id) -> Event | None:
+        """
+        Retrieves an event by its ID if the current collaborator is its support contact.
+
+        Args:
+            event_id (int): The ID of the event to retrieve.
+
+        Returns:
+            Event if the collaborator is authorized to modify it; None if the event doesn't exist.
+
+        Raises:
+            PermissionDenied: If the collaborator is not authorized to modify the event.
+        """
+        event = self.service.get_event_by_id(event_id)
+        if not event:
+            # TODO: Capture with Sentry.
+            print(f"Not event with id: {event_id} found.")
+            return None
+        if event.support_contact_id != self.collaborator.id:
+            raise PermissionDenied("You are not authorized to modify this event.")
+        return event
+
+    def modify_event(self, event_id):
+        pass
+
+    def can_user_modify_event(self, event_id) -> True | False:
+        pass
