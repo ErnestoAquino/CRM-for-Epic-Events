4f3b51484572d9482802b093f3d3f54bce9cfa93
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-12 20:58:53 +0100

    refactor: Adjust collaborator_controller constructor to follow SOLID principles

--- EpicCRM/controllers/models/collaborator_controller.py
+++ EpicCRM/controllers/models/collaborator_controller.py
@@ -1,6 +1,8 @@
 from crm.models import Collaborator
+from services.services_crm import ServicesCRM
 
 
 class CollaboratorController:
-    def __init__(self, collaborator: Collaborator):
+    def __init__(self, collaborator: Collaborator, services: ServiceCRM):
         self.collaborator = collaborator
+        self.services = services
