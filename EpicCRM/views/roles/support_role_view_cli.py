6c9b525e14082445d973ce7d27dd70b6374bb319
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-12 17:34:02 +0100

    feat: Add user prompts for event modification details

--- EpicCRM/views/roles/support_role_view_cli.py
+++ EpicCRM/views/roles/support_role_view_cli.py
@@ -1,10 +1,14 @@
-import rich.box
+from datetime import datetime
 from django.db.models.query import QuerySet
-
+from django.utils.timezone import make_aware
+from django.utils.timezone import get_default_timezone
+from dateutil.parser import parse
 import click
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
+import rich.box
+from rich.box import ROUNDED
 from colorama import Fore
 from colorama import Style
 
@@ -19,11 +23,12 @@ class SupportRoleViewCli:
         "2 - View the list of all contracts.",
         "3 - View the list of all events.",
         "4 - View your assigned events.",
-        "5 - Modify one of your assigned events."
+        "5 - Modify one of your assigned events.",
+        "6 - Exit of CRM system."
     ]
     MENU_LIMIT = len(MENU_OPTIONS)
 
-    def show_main_menu(self, collaborator: Collaborator):
+    def show_main_menu(self, collaborator: Collaborator) -> int:
         click.clear()
 
         # Get the full name or username if the full name is not available.
@@ -55,12 +60,17 @@ class SupportRoleViewCli:
         console = Console()
 
         # Create table
-        table = Table(title="List of all Clients", show_header=True, header_style="bold magenta", expand=True)
-        table.add_column("Full Name", style = "dim", width = 20)
-        table.add_column("Email", style = "dim", width = 20)
-        table.add_column("Phone", justify = "right", style = "dim", width = 12)
-        table.add_column("Company Name", style = "dim", width = 20)
-        table.add_column("Creation Date", style = "dim", width = 20)
+        table = Table(title="List of all Clients",
+                      show_header=True,
+                      header_style="bold magenta",
+                      expand=True,
+                      box=ROUNDED)
+
+        table.add_column("Full Name", style="dim", width=20)
+        table.add_column("Email", style="dim", width=20)
+        table.add_column("Phone", justify="right", style="dim", width=12)
+        table.add_column("Company Name", style="dim", width=20)
+        table.add_column("Creation Date", style="dim", width=20)
 
         # Fill the table with clients' data
         for client in clients_queryset:
@@ -184,7 +194,7 @@ class SupportRoleViewCli:
         # Print the table using Rich
         console.print(table)
 
-    def display_event_details(self, event: Event):
+    def display_event_details(self, event: Event) -> None:
         console = Console()
 
         # Create a table to display event details
@@ -257,3 +267,81 @@ class SupportRoleViewCli:
             return event_id
         else:
             click.secho("Invalid event ID. Please choose from the list", fg="red")
+
+    def prompt_for_event_modification(self) -> dict:
+        modifications = {}
+        self.display_info_message("Leave blank any field you do not wish to modify.")
+
+        new_name = click.prompt("New name for event", default="", show_default=False)
+        if new_name:
+            modifications["name"] = new_name
+
+        new_client_name = click.prompt("New client name", default="", show_default=False)
+        if new_client_name:
+            modifications["client_name"] = new_client_name
+
+        new_client_contact = click.prompt("New client contact", default="", show_default=False)
+        if new_client_contact:
+            modifications["client_contact"] = new_client_contact
+
+        new_location = click.prompt("New location", default="", show_default=False)
+        if new_location:
+            modifications["location"] = new_location
+
+        new_notes = click.prompt("New notes", default="", show_default=False)
+        if new_notes:
+            modifications["notes"] = new_notes
+
+        # Request new attendees
+        while True:
+            new_attendees_str = click.prompt("New number of attendees", default="", show_default=False)
+            if not new_attendees_str:
+                break  # Exit the loop if the user leaves the field blank
+            try:
+                new_attendees = int(new_attendees_str)
+                if new_attendees <= 0:
+                    self.display_error_message("Number of attendees cannot be negative or zero. Please try again.")
+                    continue
+                modifications["attendees"] = new_attendees
+                break  # End the loop after processing a valid number of attendees
+            except ValueError:
+                self.display_error_message("Please enter a valid integer for the number of attendees.")
+
+        # Request new start date
+        while True:
+            new_start_date_str = click.prompt("New start date (YYYY-MM-DD HH:MM)", default="", show_default=False)
+            if not new_start_date_str:
+                break  # Exit the loop if the user leaves the field blank
+            try:
+                naive_start_date = parse(new_start_date_str)
+                if naive_start_date < datetime.now():
+                    self.display_error_message("Start date must be in the future. Please try again.")
+                    continue
+                # Making date/time "aware" to comply with Django and avoid timezone warnings, ensuring correct
+                # timezone handling.
+                aware_start_date = make_aware(naive_start_date, get_default_timezone())
+                modifications["start_date"] = aware_start_date
+                break  # End the loop after processing a valid start date
+            except ValueError:
+                self.display_error_message("Invalid date format. Please use YYYY-MM-DD HH:MM.")
+
+        # Request a new end date
+        while True:
+            new_end_date_str = click.prompt("New end date (YYYY-MM-DD HH:MM)", default="", show_default=False)
+            if not new_end_date_str:
+                break  # Exit the loop if the user leaves the field blank
+            try:
+                naive_end_date = parse(new_end_date_str)
+                # Ensure that the new end date is after the new start date (if modified)
+                if "start_date" in modifications:
+                    # Making date/time "aware" to comply with Django and avoid timezone warnings.
+                    aware_end_date = make_aware(naive_end_date, get_default_timezone())
+                    if aware_end_date <= modifications["start_date"]:
+                        self.display_error_message("End date must be after start date. Please try again.")
+                        continue
+                    modifications["end_date"] = aware_end_date
+                    break  # End the loop after processing a valid end date
+            except ValueError:
+                self.display_error_message("Invalid date format. Please use YYYY-MM-DD HH:MM.")
+
+        return modifications
