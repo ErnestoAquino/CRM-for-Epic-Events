c02119515e1619374ba6e46b10d03a9730c8ec36
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-02-22 22:35:41 +0100

    refactor: Remove unnecessary imports

--- EpicCRM/controllers/main_controller_crm.py
+++ EpicCRM/controllers/main_controller_crm.py
@@ -8,10 +8,6 @@ from controllers.roles.support_role_controller import SupportRoleController
 from controllers.roles.sales_role_controller import SalesRoleController
 from controllers.roles.management_role_controller import ManagementRoleController
 
-from controllers.models.client_controller import ClientController
-from controllers.models.contract_controller import ContractController
-from controllers.models.event_controller import EventController
-
 from services.services_crm import ServicesCRM
 
 from views.main_view_cli import MainViewCLI
@@ -29,10 +25,10 @@ class MainControllerCRM:
         login_data = self.view_cli.prompt_login()
         try:
             user = self.crm_services.authenticate_collaborator(**login_data)
-            self.view_cli.print_message("Logged in successfully!", "green")
+            self.view_cli.display_info_message("Logged in successfully!")
             return user
         except ValidationError as e:
-            self.view_cli.print_message(f"Login failed: {e}", "red")
+            self.view_cli.display_error_message(f"Login failed: {e}")
             return None
 
     def present_main_menu(self, user):
@@ -40,44 +36,30 @@ class MainControllerCRM:
 
     def start(self):
         collaborator = self.authenticate_collaborator()
-        services = ServicesCRM()
 
-        # Verify that the collaborator objects exists.
+        # Verify that the collaborator objects exist.
         if collaborator is None:
-            self.view_cli.print_message("We encountered a problem during the process. Please try again.", "red")
             return
 
-        # Verify that collaborator's role in not null.
+        # Verify that collaborator's role is not null.
         if collaborator.role is not None:
             role_name = collaborator.role.name
         else:
-            self.view_cli.print_message("Your account does not have a role assigned", "yellow")
+            self.view_cli.display_warning_message("Your account does not have a role assigned")
             return
 
         match role_name:
             case "support":
-                client_controller = ClientController(collaborator, services)
-                contract_controller = ContractController(collaborator, services)
-                event_controller = EventController(collaborator, services)
                 view_cli = SupportRoleViewCli()
-
-                support_role_controller = SupportRoleController(collaborator,
-                                                                client_controller,
-                                                                contract_controller,
-                                                                event_controller,
-                                                                view_cli)
+                support_role_controller = SupportRoleController(collaborator, self.crm_services, view_cli)
                 support_role_controller.start()
             case "sales":
                 view_cli = SalesRoleViewCli()
-                sales_role_controller = SalesRoleController(collaborator,
-                                                            self.crm_services,
-                                                            view_cli)
+                sales_role_controller = SalesRoleController(collaborator, self.crm_services, view_cli)
                 sales_role_controller.start()
             case "management":
                 view_cli = ManagementRoleViewCli()
-                management_role_controller = ManagementRoleController(collaborator,
-                                                                      self.crm_services,
-                                                                      view_cli)
+                management_role_controller = ManagementRoleController(collaborator, self.crm_services, view_cli)
                 management_role_controller.start()
             case _:
-                self.view_cli.print_message("Your role does not have specific task assigned.", "yellow")
+                self.view_cli.display_warning_message("Your role does not have specific task assigned.")
